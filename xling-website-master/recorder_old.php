4d28f8f430a76b365cdff21c85c4a9b975d1cd40
soskuthy <mrhowardppa@yahoo.com>
2019-11-01 10:18:59 -0700

    deleted extra folder

--- xling-website-master/recorder_old.php
+++
@@ -1,8 +0,0 @@
-<div id="dom-target" style="display: none;">
-    <?php
-        session_start(); 
-        $user_id = "mimi"; 
-        echo $user_id; /* You have to escape because the result
-                                           will not be valid HTML otherwise. */
-    ?>
-</div>
