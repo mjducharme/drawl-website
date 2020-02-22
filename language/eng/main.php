ccf774c56318981ae04d1a9748cd8f1f398fa755
dimitri-prica <55683056+dimitri-prica@users.noreply.github.com>
2020-02-21 16:02:28 -0800

    file transfer

--- language/eng/main.php
+++ language/eng/main.php
@@ -1,48 +1,53 @@
 <?php
 	$langar = array(
-    	"IndexTitle" => "Ang Proyekto sa Crossling Corpora ",
-    	"IndexText" => "Welcome, ug salamat sa imong interes sa Crossling Corpora!<br><br>Gidapit ka namo sa pag-apil aning among pagtuon pinaagi sa pagtampo sa usa ka koleksyon sa mga pagrekord sa lainlaing sinultian. Kining mga rekording makatabang sa among pagsabot kung giunsa pagbag-o sa mga sinultian sa paglabay sa panahon, kung unsa ang kalahian sa  mga sinultian sa usag usa bahin sa mga gagmay nga detalye sa paglitok, ug kung giunsa pagrepresentar ug pagproseso sa utok ang paglitok. Kung nakahukom ka nga muapil ani nga pagtuon, hangyuon ka mga mutubag sa pipila ka mga pangutana bahin sa imong kaugalingon, irekord amg imong kaugalingon nga nagbasa og mubo nga tudling sa cebuano/ binisaya, ug pagstorya og bisan una sa parehas nga sinultian sulod sa pipila ka mga minuto. Ang tudling/pulong moabot og mga lima hantod unom ka minuto kung basahon og kusog. Para sa pagsulti og bisan unsa, hangyuon ka nga mostorya sa dili moubos sa lima ka minuto ug dili mulabaw sa napulo ka minuto.<br><br> Aron makaapil sa pagtuon, palihog i-klik ang button sa ubos ",
-    	"IndexLink" => "Muapil sa pagtuon.",
-    	"ConsentTitle" => "Consent Form/Porma sa pag-uyon: Pagtuon sa pagbag-o sa tingog sa gamay nga cross-linguistic corpora (koleksyon sa lainlain nga sinultian)",
-    	"ConsentParticipation" => "1. Pag-uyon sa pag-apil",
-    	"ConsentParticipationText" => "Kung mouyon ka nga moapil sa pagtuon, i-type ang imong pangalan ug email address sa mga box sa ubos ug i-klik ang “Mouyon ko sa pag-apil”. Ang pag-apil naglakip sa imong rekording sa among database, ang pagpaambit niini sa ubang mga tigdukiduki pinaagi sa usa ka website mga kailangan og pag-rehistro, ug pagpatik sa imong mga resulta sa pagsusi.",
-    	"ConsentName" => " Pangalan:",
-    	"ConsentEmail" => "Email:",
-    	"ConsentParticipationConsent" => " Mouyon ko sa pag-apil.",
-    	"ConsentPublication" => "2. Pag-uyon sa pagpatik sa mga kinutlo gikan sa mga rekording",
-    	"ConsentPublicationText1" => "Kung muoyon ka, ang mga kinutlo gikan sa imong mga pagrekord mahimong ipakigbahin sa publiko sa sa pagrepresentar sa mga resulta gikan sa among database. Sa tanan nga mga kaso, dili namo iupod ang inong pangalan sa mga rekording. Ikaw mahimo nga tawgan og, (sa) pananglitan, ",
-    	"ConsentPublicationText2" => ", etc. Kung muoyon ka niining mga paggamit sa mga datos, i-tick ang box sa ubos. Kung dili ka muoyon niining mga paggamit sa imomg datos, palihog ayaw i-tick ang box.",
-    	"ConsentPublicationConsent" => "Muoyon ko nga i-patik ang mga tinuko sa akong mga rekording. (Opsyonal)",
-    	"Submit" => "I-sumite",
-    	"DemoTitle" => " Ang mga pangutana sa demograpiko",
-    	"Age" => "Edad:",
-    	"Gender" => "Kinatawhan:",
-    	"PoB" => "Lugar nga natawhan:",
-    	"Location" => "Puy-anan karon:",
-    	"SpokenLanguages" => " Ubang sinultian nga matulin ka:",
-    	"Next" => "Sunod nga panid",
-    	"RecorderTitle" => "Rekording tool",
-    	"RecorderInstructionsTitle" => "Mga panudlo ",
-    	"RecorderInstructions" => "Interisado kami sa imong natural nga sinultihan. Kung mamali ka sa usa ka pulong, dili nimo kinahanglan nga i-restart an pagrekord. Balika lang ang tudling-pulong parehas sa usa ka normal nga panag-istoryahaypanag-istoryahay. Palihog og basa sa sakto nga kaapspason kung asa ka komportable. Dili na kinahanglan magpraktis bag-o magrekord.<br><br> Makit-an ang rekording tool sa tuo nga bahin. Ang nasa taas nga Rekording window gipakita ang lebel sa tingog. Ang ubos nga Rekording window ipakita ang waveform sa girekord kung mahuman na. Ang ilawom sa duwa ka window kay ang Playback Control, icon sa Mikropono, ug ang Submit icon. Para irekord ang kaugalingon, i-klik ang icon sa Mikropono (kini mosiga og pula), basaha og kusog ang pulong, dayon i-klik og usab ang icon sa Mikropono aron mahuman ang pagrekord. Ipakita dayon sa ubos nga window ang imong rekording isip usa ka waveform. Ang pag-klik sa Play button sa Playback Control magtugot kanimo nga madunggan ang imong rekording. Palihog sulayi ang pagrekord pinaagi sa paggklik sa button sa Mikropono, pagsulti sa pipila ka mga tudling-pulong, ug i-klik og usab ang button. Mahimo nimong mapaminaw ang pagsulay nga rekording sa Playback window. Kung klaro nimo nga madunggan ang imong kaugalingon ug walay kasaba sa background, mahimo nimong i-klik og usab ang button sa Mikropono (nga dili kinahanglan i-pasa/i-sumite ang gisulay nga rekording) ug magsugod na storya og kusog. Kung mahuman na ka, i-klik ang button na Mikropono aron mohunong ang pagrekord. Dayon i-click ang pagsulti og bisan unsa na tab para sa mga panudlo bahin sa ikaduwa nga rekording.<br><br> Paningkamutan namo nga maka-access  kamo pinaagi sa daghang mga platform/plataporma ug browser kutob sa mahimo. Kung ang imong browser kay [] mahimo ka makakuha og usa ka mensahe sa pagablis sa panid sa recorder. Palihog sulayi ang uban pang browser/aparato kung aduna kay magamit. Sa dihang gipasa/gisumite na nimo ang imong rekording sa tudling pagbasa, gihangyo namo nga irekord nimo ang imong kaugalingon gasulti og bisan unsa tali sa lima hangtod sa walo ka minute. Kung mahuman na ka, palihog og pasa sa/sumite rekording. Mahimo nang isarado kining window.",
-    	"RecorderReadingTitle" => " Bahin sa pagbasa: Ang Kaayuhan sa Pag-hiking",
-    	"RecorderSpontaneous" => " Pagsulti og Bisan Unsa",
-    	"RecorderList" => "<p>Mahimo nimong magamit ang mosunud nga bahin kung gusto nimo, o pagsulti og lain nga hisgot kung gusto nimo. Ikaw ang magdesisyon kung unsa imong hisgutan, apan gusto namo nga likayan nimo ang mga hisgutan nga mahimong sensitibo o kontrobersyal gikan sa mga relihiyoso / political / rasa / sekswal nga panan-aw. Gusto usab namo nga likayan nimo ang pagpadayag sa personal nga pagpaila sa kasayuran sama sa imong ngalan, adres, lugar sa trabaho ug uban pa.</p>
-	    	<ul>
-	    	<li>Ingna kami sa imong mga kalingawan</li>
-	    	<li>Unsa man ang imong mga pangandoy sa pagkabata?</li>
-	    	<li>Unsa imong pambuntag/adlaw-adlaw nga rutina?</li>
-	    	<li>Isulti kanamo ang usa ka malipayon nga panghitabo sa imong kinabuhi.</li>
-	    	<li>Unsa para kanimo ang hinungdanon sa pagkab-ot sa kalipayan?</li>
-	    	<li>Ihulagway ang imong naandan sa katapusan sa semana.</li>
-	    	<li>Unsa imong trabaho? Giunsa kini paglihok?</li>
-	    	<li>Unsa ang pinakamaayong nabuhat sa imong kinabuhi? Giunsa kini pagkaapekto sa imong kinabuhi?</li>
-	    	<li>Unsa ang pinakamapuslanon nga tambag nga imong nahatag?</li>
-	    	<li>Isulti kanamo bahin ang imong pinalabi nga libro o salida.</li>
-	    	<li>Nagustuhan nimo ang pag-iskwela? Unsa imong pinakamaayo/pinaka dili mayo nga sugpay/hisgutan?</li>
-		    	</ul>",
-		"RecorderRec" => "Sugdon/ Undangon ang Pagrekord ",
-		"RecorderSave" => "I-sumite ang rekording ",
-		"RecorderClose" => "Isarado ang Recorder ",
-		"RecorderNotYet" => "Ang rekording wala pa gipasa/gisumite",
+		"IndexTitle" => "The Crossling Corpora Project",
+		"IndexText" => "<strong>Welcome, and thank you for your interest in the Crossling Corpora!</strong><br><br>We would like to invite you to take part in our research project by contributing to a collection of speech recordings from different languages. These recordings will help us understand how languages change over time, how languages differ from each other with respect to small details of pronunciation, and, more generally, how speech is represented and processed in the brain. If you decide to participate in this study, you will be asked to answer a few brief questions about yourself, record yourself reading a short passage in [name of language], and then talking spontaneously in the same language for a few minutes. The passage will take roughly 5-6 minutes to read aloud. For the spontaneous speech section, you are asked to talk for no less than 5 minutes and no more than 10 minutes. <br><br> To participate in the experiment, please click the button below.",
+		"IndexLink" => "Participate in the experiment.",
+		"ConsentTitle" => "Consent Form: Studying sound change in small cross-linguistic corpora",
+		"ConsentParticipation" => "1. Consent to participate",
+		"ConsentParticipationText" => "If you agree to participate in the study, type your name and email address in the boxes below and tick the 'I consent to participate' box. Participation includes using your anonymized speech recordings in our database, sharing it with other researchers through a closed registration-only website, and publishing your anonymous survey results.",
+		"ConsentName" => "Name:",
+		"ConsentEmail" => "Email:",
+		"ConsentParticipationConsent" => "I consent to participate.",
+		"ConsentPublication" => "2. Consent to publish excerpts from recordings",
+		"ConsentPublicationText1" => "If you consent, excerpts from your audio recordings may be shared with the public in the presentation of results from our database. In all cases, your name will never be associated with the recordings. You may be referred to as, for example, ",
+		"ConsentPublicationText2" => ", etc. If you consent to these uses of data, please tick the box below. If you do not consent to this use of your data, please do not tick the box.",
+		"ConsentPublicationConsent" => "I consent to excerpts of my recordings being published. (Optional)",
+		"Submit" => "Submit",
+		"DemoTitle" => "Demographic questionnaire",
+		"Age" => "Age:",
+		"Gender" => "Gender:",
+		"PoB" => "Place of Birth:",
+		"Location" => "Current place of residence:",
+		"SpokenLanguages" => "Other languages that you are fluent in:",
+		"Next" => "Next page",
+		"RecorderTitle" => "Recorder Tool",
+		"RecorderInstructionsTitle" => "Instructions",
+		"RecorderInstructions" => "You can see the recording tool on the right-hand side. The top Recording window shows the level of the incoming sound. The bottom Recording window will show the recorded waveform when it is complete. Underneath the two windows are a Playback Control, a Microphone icon, and a Submit icon. To record yourself, click on the Microphone icon (which will glow red), read the passage aloud, and then click on the Microphone icon again to stop recording. The lower window will then show your recording as a waveform. Clicking on the Play button in the Playback Control will allow you to hear your recording.
+			<ol>
+			<li>Please test the recording by clicking the microphone button. It will glow red. Speak a few sentences, and click the button again. You can listen to the test recording in the playback window. Make sure you can hear yourself clearly and there are no noises in the background. <strong>Do not submit this test recording.</strong></li>
+			<li>Click the 'Reading Passage' tab and start recording by clicking the microphone button once again. When it glows red, you can start reading out loud. When you are done, click on the microphone button to stop recording, and submit your recording. Then click on the 'Spontaneous speech prompts' tab for instructions about the second recording. </li>
+			</ol> We try to be accessible through as many platforms and browsers as possible. If your browser is not supported you may get an error message when opening the recorder page. Please try with another browser/device if one is available to you.<br><br>We are interested in your natural speech. If you stumble over a word, you do not need to restart your recording. Simply repeat the sentence like you would in normal conversation. Please read at a pace you find comfortable. There is no need to rehearse before the recording.",
+		"RecorderReadingTitle" => "Reading Sample: The Benefits of Hiking",
+		"RecorderSpontaneous" => "Spontaneous Speech Prompts",
+		"RecorderList" => "<p>Make sure you have sumbitted your recording of the Recording Passage. Click the microphone button once again and start talking out loud. When you are done, click on the microphone button to stop recording, and click the submit button again. <br><br>You can use the following prompt if you wish, or talk about something else if you'd like. You are free to decide what you discuss, but we would like to ask you to avoid topics that may be sensitive or controversial from a religious / political / racial / sexual point of view. We would also like you to avoid disclosing personally identifying information such as your name, address, workplace, etc.</p>
+	        <ul>
+	        <li>Tell us about your hobbies</li>
+	        <li>What was your childhood dream?</li>
+	        <li>What is your morning/daily routine?</li>
+	        <li>Tell us about a happy event of your life.</li>
+	        <li>What do you think is important for achieving happiness?</li>
+	        <li>Describe your typical weekend.</li>
+	        <li>What's your job? How does it work?</li>
+	        <li>What's your life's greatest achievement? How has this affected your life?</li>
+	        <li>What is the most useful piece of advice you have been given?</li>
+	        <li>Tell us about your favourite book or movie.</li>
+	        <li>Did you like school? What were your best/worst subjects?</li>
+	    	</ul>
+	    	<p>Once you have spoken for 5-8 minutes, please end your recording, and submit it using the submit button. It is now safe to close this window.</p>",
+	    "RecorderRec" => "Start/Stop Recording",
+	    "RecorderSave" => "Submit Recording",
+	    "RecorderClose" => "Close Recorder",
+	    "RecorderNotYet" => "Recording not yet submitted",
 	);
 ?>
