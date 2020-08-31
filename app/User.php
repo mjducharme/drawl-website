04ddf2ffef026cbafa5623982d7926ca33b60d9d
Michael Ducharme <mducharme@gmail.com>
2020-08-31 03:21:01 -0700

    Reverted User model changes

--- app/User.php
+++ app/User.php
@@ -5,12 +5,10 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Okipa\LaravelModelJsonStorage\ModelJsonStorage;
 
 class User extends Authenticatable
 {
     use Notifiable;
-    use ModelJsonStorage;
 
     /**
      * The attributes that are mass assignable.
