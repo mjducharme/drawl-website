84ddebd611000593ff9a7bf62b176bb0a2f6a6a4
Michael Ducharme <mducharme@gmail.com>
2020-09-21 14:23:57 -0700

    Fixed scaling on mobile

--- resources/views/layouts/wizard.blade.php
+++ resources/views/layouts/wizard.blade.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 	<script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 	<script src="{{ asset('js/jquery.modal-wizard.min.js') }}"></script>
 	<script src="https://kit.fontawesome.com/535513208d.js" crossorigin="anonymous"></script>
     @yield('extra-headers')
-<meta charset="UTF-8">
-
 <title>
 	@lang('messages.IndexTitle') &nbsp;>&nbsp; @yield('title')
 </title>
