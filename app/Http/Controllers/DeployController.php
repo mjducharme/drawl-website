a029d538cfd4afb32b1b9f0df0924946217af2fd
Michael Ducharme <mducharme@gmail.com>
2020-09-21 19:39:06 -0700

    test

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -22,7 +22,8 @@ class DeployController extends Controller
         $root_path = base_path();
         $process = new Process(['/bin/sh','../deploy.sh']);
         $process->run(function ($type, $buffer) {
-            echo $buffer;
+            return response('Hello World', 200)
+                  ->header('Content-Type', 'text/plain');
         });
         
     }
