940d699151b5eb43a622e6ded5aa9e3986908d23
Michael Ducharme <mducharme@gmail.com>
2020-09-22 02:53:16 -0700

    No ansi in composer deployment

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ php artisan down
 git pull
 
 # update PHP dependencies
-composer install --no-interaction --no-dev --prefer-dist
+composer install --no-interaction --no-dev --prefer-dist --no-ansi
 # --no-interaction Do not ask any interactive question
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
