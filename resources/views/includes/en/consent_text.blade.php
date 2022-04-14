541621fa1378855f8d5775c457fa1ec7c8cb6b5f
Michael Ducharme <mducharme@gmail.com>
2022-04-14 12:55:59 -0700

    Updated Consent Text

--- resources/views/includes/en/consent_text.blade.php
+++ resources/views/includes/en/consent_text.blade.php
@@ -1,11 +1,13 @@
-<p>This project is about discovering different regional dialects in British Columbia. If you chose to participate in this study, you will be asked to read a short story over the phone or through the browser on your computer, in addition to providing some demographic and personal data about your language use and history. We will analyze your speech patterns to better understand the dialect landscape in Western Canada.</p>
+<p>This project is about discovering different regional dialects in British Columbia. If you chose to participate in this study, you will be asked to read a short story over the phone or through the browser on your computer, in addition to providing some demographic and personal data about your language use and history. We will analyze your speech patterns to better understand the dialect landscape in Western Canada. This project is supported by the Social Sciences and Research Council of Canada.</p>
 
 <p>This study is being conducted by Dr. Molly Babel and Dr. Bob Pritchard, who are both professors at the University of British Columbia. If you have questions about the study, please contact Molly at molly.babel@ubc.ca or Bob at bob@mail.ubc.ca.</p>
 
 <p>Your participation is entirely voluntary and optional. If you choose to participate in this study, your doing so is confidential and we do not collect identifying information. By agreeing to participate in this study you agree that your audio recording and its associated accent analysis can become part of the project accent database. Your recordings will be edited to be anonymous, and can be used for associated research purposes, either by being shared with other researchers, made publicly available as part of a dataset, or used in the creation of an artistic work exploring different accents.</p>
 
-<p>If you participate in this study you are eligible to receive a CD of performances by UBC ensembles, or tickets to UBC concerts while supplies last. While there are no other direct benefits to you in participating, there are also no risks.</p>
+<p>There are no direct benefits or risks to participating in this study.</p>
 
 <p>If you have any concerns or complaints about your rights as a research participant and/or your experiences while participating in this study, contact the Research Participant Complaint Line in the UBC Office of Research Ethics at 604-822-8598, or if long distance, email RSIL@ors.ubc.ca or call toll free 1-877-822-8598.</p>
 
 <p>By advancing to the next page, you are consenting to participate in this research.</p>
+
+<p>UBC BREB: H17-02886</p>
